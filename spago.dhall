5b3d908f2a7b2254913fa0f55c5b7f2aeaee727b
Viktor Lin <hi@viktorl.in>
2020-09-25 09:06:27 +0000

    init

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+    [ "console", "effect", "halogen", "halogen-css", "psci-support" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,33 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.VDom.Driver (runUI)
+
+data State = Number
+
+data Action = Def
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI component unit body
+
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+    where
+  initialState _ = 10.0
+
+  render state =
+    HH.div []
+           [ HH.div []
+                    [ HH.textarea []  ]
+           ]
+  
+  handleAction = case _ of _ -> H.modify_ \s -> s
