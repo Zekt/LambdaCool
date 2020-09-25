922e677b3bc1f6f0e781ba149577d8355ed4b0a5
Viktor Lin <hi@viktorl.in>
2020-09-25 18:37:40 +0800

    trying things

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,13 @@ module Main where
 import Prelude
 
 import Effect (Effect)
+import CSS as CSS
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.CSS as HC
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 
 data State = Number
@@ -27,9 +31,14 @@ component =
   initialState _ = 10.0
 
   render state =
-    HH.div []
-           [ HH.div []
-                    [ HH.textarea []  ]
-           ]
+      HH.div [] [
+        HH.div [ HC.style containerStyle ]
+               [ HH.div []
+                        [ HH.textarea [ HC.style textareaStyle ] ]
+               ]
+      ]
   
   handleAction = case _ of _ -> H.modify_ \s -> s
+
+containerStyle = do CSS.border CSS.solid (CSS.px 1.0) CSS.gray
+textareaStyle = do CSS.textWhitespace CSS.whitespaceNoWrap
