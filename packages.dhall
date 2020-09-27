75cc0100c62f796e4e6e6ee7072b9e8f0ba42323
Viktor Lin <hi@viktorl.in>
2020-09-28 02:13:13 +0800

    not working

--- packages.dhall
+++ packages.dhall
@@ -119,7 +119,7 @@ let additions =
 
 
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.13.6-20200507/packages.dhall sha256:9c1e8951e721b79de1de551f31ecb5a339e82bbd43300eb5ccfb1bf8cf7bbd62
+      https://github.com/purescript/package-sets/releases/download/psc-0.13.8-20200922/packages.dhall sha256:5edc9af74593eab8834d7e324e5868a3d258bbab75c5531d2eb770d4324a2900
 
 let overrides = {=}
 
--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,14 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
+
 import CSS as CSS
+import CSS.Common as CSS.Common
+import CSS.TextAlign as CSS.TextAlign
+import CSS.Overflow as CSS.Overflow
+
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
@@ -12,33 +18,75 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 
-data State = Number
+import DOM (DOM)
+import DOM.HTML (window) as DOM
+import DOM.HTML.HTMLInputElement (validity) as DOM
+import DOM.HTML.Types
+  (ValidityState, htmlDocumentToNonElementParentNode, readHTMLInputElement) as DOM
+import DOM.HTML.Window (document) as DOM
+import DOM.Node.NonElementParentNode (getElementById) as DOM
 
-data Action = Def
+cssRule :: String -> String -> CSS.CSS
+cssRule x y = CSS.rule (CSS.Property (CSS.Key $ CSS.Plain x) (CSS.Value $ CSS.Plain y))
+
+
+getElementById
+    :: forall a eff
+     . (Foreign -> F a)
+    -> String
+    -> Eff (dom :: DOM | eff) (Maybe a)
+getElementById reader elementId =
+    DOM.window
+    >>= DOM.document
+    <#> DOM.htmlDocumentToNonElementParentNode
+    >>= DOM.getElementById (wrap elementId)
+    <#> (_ >>= runReader reader)
+
+data State = String
+
+data Action = Update String
 
 main :: Effect Unit
 main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI component unit body
+    body <- HA.awaitBody
+    runUI component unit body
 
 component =
-  H.mkComponent
+    H.mkComponent
     { initialState
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
     where
-  initialState _ = 10.0
+      initialState _ = ""
 
   render state =
-      HH.div [] [
-        HH.div [ HC.style containerStyle ]
-               [ HH.div []
-                        [ HH.textarea [ HC.style textareaStyle ] ]
-               ]
+  --let parsedState = parse state
+    HH.div [] [
+      HH.div [ HC.style containerStyle ]
+      [ HH.div []
+               [ HH.textarea [ HE.onKeyDown \_ -> Just (Update s)
+               , HC.style textareaStyle ] ]
+      , HH.span [] [ HH.text state ]
       ]
-  
-  handleAction = case _ of _ -> H.modify_ \s -> s
+    ]
 
-containerStyle = do CSS.border CSS.solid (CSS.px 1.0) CSS.gray
-textareaStyle = do CSS.textWhitespace CSS.whitespaceNoWrap
+  handleAction = case _ of
+                      Update s -> do getElement
+                                     H.modify_ \_ -> s
+
+containerStyle = do CSS.width $ CSS.Size $ CSS.Value $ CSS.Plain "100%"
+    CSS.height $ CSS.Size $ CSS.Value $ CSS.Plain "100%"
+    CSS.display CSS.flex
+    CSS.justifyContent CSS.Common.center
+    CSS.alignItems CSS.Common.center
+    CSS.TextAlign.textAlign CSS.TextAlign.center
+    CSS.Overflow.overflow CSS.Overflow.hidden
+
+
+textareaStyle = do CSS.position CSS.absolute
+    cssRule "opacity" "1"
+    --cssRule "pointer-events" "none"
+    --CSS.TextAlign.textAlign CSS.TextAlign.center
+    CSS.textWhitespace CSS.whitespaceNoWrap
+
